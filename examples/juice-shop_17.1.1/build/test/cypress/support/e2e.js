4e74cbc9fe0cf2cf9ca4d29630c6601c9a160429
o029937m@student.staffs.ac.uk <Wale Oseni>
2025-01-16 03:31:14 +0000

    Sync changes from Azure DevOps

--- examples/juice-shop_17.1.1/build/test/cypress/support/e2e.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./commands");
-require("./setup");
-//# sourceMappingURL=e2e.js.map
--- examples/juice-shop_17.1.1/build/test/cypress/support/setup.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-beforeEach(() => {
-    cy.setCookie('cookieconsent_status', 'dismiss');
-    cy.setCookie('welcomebanner_status', 'dismiss');
-    cy.setCookie('language', 'en');
-});
-//# sourceMappingURL=setup.js.map
